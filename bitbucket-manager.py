72a12e3efa3d69a69aec5329056baf8521137eb1
John Smith <john.smith@aligent.com.au>
2021-11-18 13:36:30 +1030

    Add set-environment-variable command to arg parser

--- bitbucket-manager.py
+++ bitbucket-manager.py
@@ -26,27 +26,62 @@ PASSWORD = os.getenv('BITBUCKET_PASSWORD') if os.getenv('BITBUCKET_PASSWORD') el
 AUTH=HTTPBasicAuth(USER, PASSWORD)
 
 parser = argparse.ArgumentParser(description='List the content of a folder')
-parser.add_argument('command',
-                    metavar='command',
-                    choices=['create', 'audit'],
-                    type=str,
-                    help='The command to run')
+subparsers = parser.add_subparsers(help='sub-command help', dest="cmd")
 
-parser.add_argument('-r', '--repository-name',  
-                    required=True,
+create_parser = subparsers.add_parser('create-repository', help='create-repository help')
+
+create_parser.add_argument('repository',
+                    type=str,
                     help='The name of the repository')
 
-parser.add_argument('-w', '--workspace',
+create_parser.add_argument('workspace',
+                    type=str,
                     help='The name of the target workspace')
 
-parser.add_argument('-p', '--project',
+create_parser.add_argument('project',
+                    type=str,
                     help='The name of the target project')
+
+
+set_env_variable_parser = subparsers.add_parser('set-environment-variable', help='set-environment-variable help')
+
+set_env_variable_parser.add_argument('repository',
+                    type=str,
+                    help='The name of the repository')
+
+set_env_variable_parser.add_argument('workspace',
+                    type=str,
+                    help='The name of the target workspace')
+
+set_env_variable_parser.add_argument('environment',
+                    type=str,
+                    help='The deployment environment')
+
+set_env_variable_parser.add_argument('key',
+                    type=str,
+                    help='The variable key')
+
+set_env_variable_parser.add_argument('value',
+                    type=str,
+                    help='The variable value')
+
+set_env_variable_parser.add_argument('-s', '--sensitive',
+                    action='store_true',
+                    help='Whether the variable is sensitive or not')
 
 args = parser.parse_args()
 
-if args.command == "create":
-    print(f"Creating new repository {args.repository_name}")
+if args.cmd == "create-repository":
+    print(f"Creating new repository {args.repository}")
     bitbucket = Bitbucket(AUTH, CONFIG)
-    bitbucket.create_and_configure_repository(args.repository_name, args.project, args.workspace);
+    bitbucket.create_and_configure_repository(args.repository, args.project, args.workspace);
+elif args.cmd == "set-environment-variable":
+    print(f"Setting deployment environment variable {args.key}")
+    bitbucket = Bitbucket(AUTH, CONFIG)
+    uuid = bitbucket.get_uuid_for_environment(args.repository, args.workspace, args.environment)
+    if uuid is None:
+        print(f"Could not find deployment environment {args.environment}")
+        exit(-1)
+    bitbucket.set_deployment_environment_variable(args.repository, args.workspace, uuid, args.key, args.value, args.sensitive)
 else:
     print(F"Command {args.command} not implemented")
